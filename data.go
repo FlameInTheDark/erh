1ba509d98a41a724be19186afbf6138323dd3190
FlameInTheDark <viktorfreedom@gmail.com>
2022-01-07 20:30:50 +0500

    some documentation

--- data.go
+++ data.go
@@ -1,31 +1,37 @@
 package erh
 
+//Motd message of the day
 type Motd struct {
 	Msg string `json:"msg"`
 	Url string `json:"url"`
 }
 
+//Symbol contains currency code and description
 type Symbol struct {
 	Description string `json:"description"`
 	Code        string `json:"code"`
 }
 
+//SymbolsResponse contains response of the symbols method
 type SymbolsResponse struct {
 	Motd    Motd              `json:"motd"`
 	Success bool              `json:"success"`
 	Symbols map[string]Symbol `json:"symbols"`
 }
 
+//ConvertQuery contains query arguments of convert method
 type ConvertQuery struct {
 	From   string  `json:"from"`
 	To     string  `json:"to"`
 	Amount float64 `json:"amount"`
 }
 
+//ConvertInfo contains converted data
 type ConvertInfo struct {
 	Rate float64 `json:"rate"`
 }
 
+//ConvertResponse contains response of convert method
 type ConvertResponse struct {
 	Motd       Motd         `json:"motd"`
 	Success    bool         `json:"success"`
@@ -36,6 +42,7 @@ type ConvertResponse struct {
 	Result     float64      `json:"result"`
 }
 
+//TimeSeriesResponse contains rime series response
 type TimeSeriesResponse struct {
 	Motd       Motd                          `json:"motd"`
 	Success    bool                          `json:"success"`
@@ -46,6 +53,7 @@ type TimeSeriesResponse struct {
 	Rates      map[string]map[string]float64 `json:"rates"`
 }
 
+//HistoricalResponse contains historical response
 type HistoricalResponse struct {
 	Motd       Motd               `json:"Motd"`
 	Success    bool               `json:"success"`
@@ -55,6 +63,7 @@ type HistoricalResponse struct {
 	Rates      map[string]float64 `json:"rates"`
 }
 
+//LatestResponse contains latest rates response
 type LatestResponse struct {
 	Motd    Motd               `json:"motd"`
 	Success bool               `json:"success"`
